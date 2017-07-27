eb8b3a51620d98f1cd141d0a75fb40ec2afaa7c2
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-27 12:18:20 -0500

    Collisions, Meshes, etc.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Vector3 InitialVelocity = new Vector3(0,0,100f);
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+       
+        Move();
+	}
+
+    public void Move()
+    {
+        audioSource.Play();
+        rigidbody.velocity =  InitialVelocity;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+      
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Ball ball;
+    public float HeadPinPos;
+    private Vector3 DistanceCameraBall;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DistanceCameraBall = transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        if(ball.transform.position.z<=HeadPinPos)
+            transform.position=ball.transform.position + DistanceCameraBall;
+    }
+}
