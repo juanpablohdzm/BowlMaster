a9ced9f000e30b5644d6ff5eee0a2700fad80e22
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-28 14:28:20 -0500

    Test tools

---
+++ Assets/Editor/ScoreMasterTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+[TestFixture]
+public class ScoreMasterTest
+{
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    [Test]
+    public void FailingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01FirstStrikeReturnsEndTurn()
+    {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn,actionMaster.Bowl(10));
+    }
+
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster
+{
+
+     public enum Action { Tidy,Reset,EndTurn,EndGame};
+     public Action Bowl(int pins)
+     {
+        if (pins < 0 || pins > 10)
+            throw new UnityException("Not valid pin number");
+        if(pins==10)
+        return Action.EndTurn;
+        return Action.Tidy;
+     }
+}
