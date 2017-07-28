7ea79d9df2f684a91d96e071f4e0c08d74eb5917
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-28 18:00:35 -0500

    Test Development Drive Finish

--- Assets/Editor/ScoreMasterTest.cs
+++ Assets/Editor/ScoreMasterTest.cs
@@ -10,8 +10,8 @@ public class ScoreMasterTest
     private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
     private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
 
-  
     [SetUp]
     public void Setup()
     {
@@ -45,6 +45,51 @@ public class ScoreMasterTest
     {
         actionMaster.Bowl(2);
         Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T05CheckResetAtStrikeInLastFrame()
+    {
+        
+        for (int i = 0; i < 18; i++)
+        {
+            actionMaster.Bowl(1);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T06CheckResetSpareAtLastFrame()
+    {
+        for (int i = 0; i < 18; i++)
+        {
+            actionMaster.Bowl(1);
+        }
+        actionMaster.Bowl(1);
+        Assert.AreEqual(reset, actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T07CheckEndGameAtLastFrame()
+    {
+        for (int i = 0; i < 18; i++)
+        {
+            actionMaster.Bowl(1);
+        }
+         actionMaster.Bowl(1);
+         actionMaster.Bowl(7);
+        Assert.AreEqual(ActionMaster.Action.EndGame, actionMaster.Bowl(2));
+    }
+
+    [Test]
+    public void T08GameEndsAtBowl20()
+    {
+
+        for (int i = 0; i < 19; i++)
+        {
+            actionMaster.Bowl(1);
+        }
+        Assert.AreEqual(ActionMaster.Action.EndGame, actionMaster.Bowl(1));
     }
 }
 
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class ActionMaster
 {
     public enum Action { Tidy,Reset,EndTurn,EndGame};
+
+    private int[] bowls = new int[21];
     private  int bowl = 1;
 
      public Action Bowl(int pins)
      {
         if (pins < 0 || pins > 10)
             throw new UnityException("Not valid pin number");
+
+        bowls[bowl-1] = pins;
+        if (bowl>=19 && Bowl21Awarded())
+        {
+            bowl++;
+            return Action.Reset;
+        }
+        else 
+            if(bowl==20 && !Bowl21Awarded())
+            {
+            return Action.EndGame;
+            }
+
+        if (bowl == 21)
+            return Action.EndGame;
+
         if (pins == 10)
         {
+           
             bowl += 2;
             return Action.EndTurn;
             
@@ -20,6 +39,7 @@ public class ActionMaster
 
         if (bowl % 2 != 0)
         {
+           
             bowl ++;
             return Action.Tidy;
                    
@@ -32,4 +52,8 @@ public class ActionMaster
             }
         throw new UnityException("Need code");
      }
+    private bool Bowl21Awarded()
+    {
+        return (bowls[18] + bowls[19] >= 10);
+    }
 }
