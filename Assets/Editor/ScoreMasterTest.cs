4184f220046b2a5867ba819e6dd99ede7facca15
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-28 17:20:29 -0500

    Control Code

--- Assets/Editor/ScoreMasterTest.cs
+++ Assets/Editor/ScoreMasterTest.cs
@@ -7,7 +7,19 @@ using System.Collections;
 [TestFixture]
 public class ScoreMasterTest
 {
+    private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+
+  
+    [SetUp]
+    public void Setup()
+    {
+        actionMaster = new ActionMaster();
+    }
+        
+    
+
     [Test]
     public void FailingTest()
     {
@@ -17,8 +29,25 @@ public class ScoreMasterTest
     [Test]
     public void T01FirstStrikeReturnsEndTurn()
     {
-        ActionMaster actionMaster = new ActionMaster();
-        Assert.AreEqual(endTurn,actionMaster.Bowl(10));
+
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
     }
 
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03SpareReturnsEndTurn()
+    {
+        actionMaster.Bowl(2);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+    }
 }
+
+   
+
+
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class ActionMaster
 {
+    public enum Action { Tidy,Reset,EndTurn,EndGame};
+    private  int bowl = 1;
 
-     public enum Action { Tidy,Reset,EndTurn,EndGame};
      public Action Bowl(int pins)
      {
         if (pins < 0 || pins > 10)
             throw new UnityException("Not valid pin number");
-        if(pins==10)
-        return Action.EndTurn;
-        return Action.Tidy;
+        if (pins == 10)
+        {
+            bowl += 2;
+            return Action.EndTurn;
+            
+        }
+
+        if (bowl % 2 != 0)
+        {
+            bowl ++;
+            return Action.Tidy;
+                   
+        }
+        else 
+            if(bowl % 2==0)
+            {
+                bowl++;
+                return Action.EndTurn;
+            }
+        throw new UnityException("Need code");
      }
 }
