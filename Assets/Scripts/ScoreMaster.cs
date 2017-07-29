c632d34071ad8c6a26114d5b81403050debaa8a1
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-29 12:41:34 -0500

    start refactor

---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster
+{
+    public  static List<int> ScoreFrames(List<int> rolls)
+    {
+        List<int> framelist = new List<int>();
+
+
+
+        return framelist;
+    }
+
+}
