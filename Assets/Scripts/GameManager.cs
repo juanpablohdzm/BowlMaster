bacf83b9530e9b50d40ac711052c3794ac9a840f
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-30 13:35:50 -0500

    Array of UI text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,13 @@ public class GameManager : MonoBehaviour {
     private List<int> bowls = new List<int>();
     private PinSetter pinSetter;
     private Ball ball;
+    private ScoreDisplay scoreDisplay;
 	// Use this for initialization
 	void Start () {
         pinSetter = GameObject.FindObjectOfType<PinSetter>();
         ball= GameObject.FindObjectOfType<Ball>();
+        scoreDisplay = GameObject.FindObjectOfType<ScoreDisplay>();
+
     }
 	
 	public void Bowl(int pinFall)
@@ -17,6 +20,7 @@ public class GameManager : MonoBehaviour {
         bowls.Add(pinFall);
         ActionMaster.Action nextAction = ActionMaster.NextAction(bowls);
         pinSetter.Animation(nextAction);
+
         ball.BallReset();
     }
 }
